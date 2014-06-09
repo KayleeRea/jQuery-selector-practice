3d1a513e781d0b66f16028aa2b72e4a1faacbce7
Kaylee Edmonson <kaylee.edmonson@gmail.comcore.autocrlf>
2014-06-09 09:38:15 -0600

    jQuery practice

--- js/warmup.js
+++ js/warmup.js
@@ -12,38 +12,38 @@ window.Warmup = {
       Description: show the names of the people at the second level
       Outcome: Joel, Aaliyah, Claude
     */
-    showNamesForSelector("#second-level-button", ___);
+    showNamesForSelector("#second-level-button", "#root > li > span > span");
     //                       You need to replace ^^^ with a valid CSS selector
 
     /*
       Description: show the names of the people who have data-employment="full-time"
       Outcome: Izaiah, Della, Lowell
     */
-    showNamesForSelector("#full-time-button", ___);
+    showNamesForSelector("#full-time-button",".data-employment=full-time");
 
     /*
       Description: show the names of the people with the class name "talkative"
       Outcome: Claude, Lurline
     */
-    showNamesForSelector("#talkative-button", ___);
+    showNamesForSelector("#talkative-button", ".talkative");
 
     /*
       Description: show the names of the people who have an id of "vp"
       Outcome: Eulalia
     */
-    showNamesForSelector("#vp-button", ___);
+    showNamesForSelector("#vp-button", "#vp");
 
     /*
       Description: show the names of the people under Isaiah
-      Outcome: Della, Lurline, Aaliyah, Elisa
+      Outcome: Della, Lurline, Aaliyah, Elisa, Joel
     */
-    showNamesForSelector("#under-isaiah-button", ___);
+    showNamesForSelector("#under-isaiah-button", "");
 
     /*
       Description: show the names of the people under Eulalia
       Outcome: Claude, Lowell
     */
-    showNamesForSelector("#under-eulalia-button", ___);
+    showNamesForSelector("#under-eulalia-button", "#vp > span");
 
     /*
       Description: values of all of the input fields of type text
